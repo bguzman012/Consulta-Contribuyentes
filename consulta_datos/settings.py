055d00992972dde0980cff56a0fb7d9b84c4bd6f
Bryam Wilson Guzman Cabrera <bguzman@ups.edu.ec>
2023-07-24 15:42:49 -0500

    Cambio settings selenium

--- consulta_datos/settings.py
+++ consulta_datos/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'consulta_datos.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'contribuyentes',
+        'USER': 'odoo',
+        'PASSWORD': 'odoo',
+        'HOST': '172.16.100.52',
+        'PORT': '5432',
     }
 }
 
